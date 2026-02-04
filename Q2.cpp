b01e08e2a03a1ca619fe51792e2083a03d433c0a
manan25csu136-glitch <manan25csu136@ncuindia.edu>
2026-02-04 13:55:41 +0530

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int radius;
+    cout<<"Enter  Radius of  Fountain"<<endl;
+    cin>>radius;
+    float area=3.14*radius*radius;
+    cout<<"The Area of  Fountain is= "<<area;
+
+    return 0;
+}
