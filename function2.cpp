ec21eae83d237b596adbd8447dfb031783de6463
manan25csu136-glitch <manan25csu136@ncuindia.edu>
2026-03-24 23:31:00 +0530

    Add files via upload

---
+++ function2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int result(int s1,int s2,int s3=0,int s4=0,int s5=0)
+
+    {
+        int result(int s1,int s2,int s3,int s4,int s5);
+        return (s1+s2+s3+s4+s5);
+    }
+    int main() {
+        cout<<"result of 2 subjects is "<<result(45,58)<<endl;
+        cout<<"result of 3 subjects is "<<result(57,68,95)<<endl;
+        cout<<"result of 4 subjects is "<<result(78,49,65,77)<<endl;
+        cout<<"result of 5 subjects is "<<result(45,69,84,56,75)<<endl;
+        return 0;
+    }
+
+        
+    
+
+    
+
+
+
+
+    
